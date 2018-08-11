f4cd5d6fd968c340aa65c4a2322801a68f26be5f
Sam Lynch <sam.lynch2@hotmail.co.uk>
2018-08-11 17:18:27 +0200

    v1.1.0 - Implement directory decompiling

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const TEXTS_PROPS = {
 	UNK3: 5
 };
 
-function main(){
-	if(process.argv.length < 4) {
+function main() {
+	if (process.argv.length < 4) {
 		console.log(`
 			PSBTranslator v1.0.0
 			Usage: 
@@ -21,67 +21,76 @@ function main(){
 	}
 
 	const filename = process.argv[3];
-	if(process.argv[2] === 'decompile') {
-		decompile(filename);
-	} else if(process.argv[2] === 'recompile') {
-		recompile(filename);
+	const isDirectory = fs.lstatSync(filename).isDirectory();
+	if(isDirectory) console.log("Path is directory");
+
+	if (process.argv[2] === 'decompile') {
+		decompile(filename, isDirectory);
+	} else if (process.argv[2] === 'recompile') {
+		recompile(filename, isDirectory);
 	} else {
 		console.log('Invalid parameters');
 	}
 }
 
-function recompile(filename) {
-  // file is split by new lines
-  // remove empty lines
-  // Name: Text format
-  // In chronological order
-  // Load original json
+function recompile(filename, isDirectory) {
+	if(isDirectory){
+		console.log(`Recompiling files inside './${filename}'...`);
+		const fileList = fs.readdirSync(filename);
+		for(let i = 0; i < fileList.length; i++){
+			const fileListFileName = `${filename}/${fileList[i]}`;
+			recompile(fileListFileName);
+		}
+		return;
+	}
 
-	if(!fs.existsSync(filename)) {
+	console.log(`Recompiling ${filename}...`);
+
+	if (!fs.existsSync(filename)) {
 		console.error(`
 			File ${filename} cannot be found!
 		`);
 		return;
-  }
-  
-	if(!fs.existsSync(filename.substr(0,filename.length - 4))) {
+	}
+
+	if (!fs.existsSync(filename.substr(0, filename.length - 4))) {
 		console.error(`
-			File ${filename.substr(0,filename.length - 4)} cannot be found!
+			File ${filename.substr(0, filename.length - 4)} cannot be found!
 		`);
 		return;
 	}
 
 	const file = fs.readFileSync(filename, 'utf8');
-	const fileJson = JSON.parse(fs.readFileSync(filename.substr(0,filename.length - 4), 'utf8'));
+	const fileJson = JSON.parse(fs.readFileSync(filename.substr(0, filename.length - 4), 'utf8'));
 
-  const fileSplit = file.split('\n');
-  const lines = [];
-  for(let i = 0; i < fileSplit.length; i++){
-    let currLine = fileSplit[i];
-    currLine = currLine.trim();
-    if(currLine === '') continue;
-    else {
-      lines.push(currLine);
-    }
-  }
+	const fileSplit = file.split('\n');
+	const lines = [];
+	for (let i = 0; i < fileSplit.length; i++) {
+		let currLine = fileSplit[i];
+		currLine = currLine.trim();
+		if (currLine === '') continue;
+		else {
+			lines.push(currLine);
+		}
+	}
 
-  for(let i = 0; i < lines.length; i++){
-    let currLine = lines[i];
-    const name = currLine.substr(0, currLine.indexOf(':'));
-    const text = currLine.substr(currLine.indexOf(': ') + 2);
-    lines[i] = {
-      name: name,
-      text: text
-    };
-  }
+	for (let i = 0; i < lines.length; i++) {
+		let currLine = lines[i];
+		const name = currLine.substr(0, currLine.indexOf(':'));
+		const text = currLine.substr(currLine.indexOf(': ') + 2);
+		lines[i] = {
+			name: name,
+			text: text
+		};
+	}
 
-	for(let s = 0; s < fileJson.scenes.length; s++) {
-    const currScn = fileJson.scenes[s];
+	for (let s = 0; s < fileJson.scenes.length; s++) {
+		const currScn = fileJson.scenes[s];
 
-		for(let t = 0; t < currScn.texts.length; t++) {
-      const calculatedIndex = (fileJson.scenes.length * s) + t;
+		for (let t = 0; t < currScn.texts.length; t++) {
+			const calculatedIndex = (fileJson.scenes.length * s) + t;
 			const currTxt = currScn.texts[calculatedIndex];
-			if(currTxt[TEXTS_PROPS.NAME2] !== null) {
+			if (currTxt[TEXTS_PROPS.NAME2] !== null) {
 				currTxt[TEXTS_PROPS.NAME2] = lines[calculatedIndex].name;
 			} else {
 				currTxt[TEXTS_PROPS.NAME1] = lines[calculatedIndex].name;
@@ -91,8 +100,8 @@ function recompile(filename) {
 	}
 
 	try {
-		fs.writeFileSync(`${filename.substr(0,filename.length - 9)}_new.json`, JSON.stringify(fileJson, undefined, '\t'), 'utf8');
-	} catch(err) {
+		fs.writeFileSync(`${filename.substr(0, filename.length - 9)}_new.json`, JSON.stringify(fileJson, undefined, '\t'), 'utf8');
+	} catch (err) {
 		console.error('Failed to write file because ' + err);
 		return;
 	}
@@ -100,8 +109,20 @@ function recompile(filename) {
 	console.log('Success!');
 }
 
-function decompile(filename){
-	if(!fs.existsSync(filename)) {
+function decompile(filename, isDirectory) {
+	if(isDirectory){
+		console.log(`Decompiling files inside './${filename}'...`);
+		const fileList = fs.readdirSync(filename);
+		for(let i = 0; i < fileList.length; i++){
+			const fileListFileName = `${filename}/${fileList[i]}`;
+			decompile(fileListFileName);
+		}
+		return;
+	}
+
+	console.log(`Decompiling ${filename}...`);
+
+	if (!fs.existsSync(filename)) {
 		console.error(`
 			File ${filename} cannot be found!
 		`);
@@ -113,7 +134,7 @@ function decompile(filename){
 	let fileJson = createTemplateJson();
 	try {
 		fileJson = JSON.parse(file);
-	} catch(err) {
+	} catch (err) {
 		console.error(`
 			File ${filename} is not valid json!
 		`);
@@ -123,15 +144,15 @@ function decompile(filename){
 	const result = [];
 	let resultStr = '';
 
-	for(let s = 0; s < fileJson.scenes.length; s++) {
+	for (let s = 0; s < fileJson.scenes.length; s++) {
 		const currScn = fileJson.scenes[s];
 
-		for(let t = 0; t < currScn.texts.length; t++) {
+		for (let t = 0; t < currScn.texts.length; t++) {
 			const currTxt = currScn.texts[t];
 			const currLine = {name: '', text: ''};
 			const name1 = currTxt[TEXTS_PROPS.NAME1];
 			const name2 = currTxt[TEXTS_PROPS.NAME2];
-			if(name2 !== null) {
+			if (name2 !== null) {
 				currLine.name = name2;
 			} else {
 				currLine.name = name1;
@@ -141,13 +162,13 @@ function decompile(filename){
 		}
 	}
 
-	for(let i = 0; i < result.length; i++){
+	for (let i = 0; i < result.length; i++) {
 		resultStr += `${result[i].name}: ${result[i].text}\n\n`;
 	}
 
 	try {
 		fs.writeFileSync(`${filename}.txt`, resultStr, 'utf8');
-	} catch(err) {
+	} catch (err) {
 		console.error('Failed to write file because ' + err);
 		return;
 	}
@@ -155,7 +176,7 @@ function decompile(filename){
 	console.log('Success!');
 }
 
-function createTemplateJson(){
+function createTemplateJson() {
 	return {
 		hash: '',
 		name: '',
@@ -164,7 +185,7 @@ function createTemplateJson(){
 	};
 }
 
-function createTemplateScene(){
+function createTemplateScene() {
 	return {
 		firstLine: 0,
 		label: '',
